716c46888989db5e82536c043e7d1588c32043dd
Denis Žoljom <denis.zoljom@gmail.com>
2020-12-26 11:03:30 +0100

    Add a named request parameter for a hello route

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,23 +3,22 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
 	/**
-	 * @Route("/", name="homepage")
+	 * @Route("/hello/{name}", name="homepage")
 	 *
-	 * @param Request $request
-	 * 
+	 * @param string $name
+	 *
 	 * @return Response
 	 */
-	public function index(Request $request): Response
+	public function index(string $name): Response
 	{
 		$greet = '';
-		if ($name = $request->query->get('hello')) {
+		if ($name) {
 			$greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
 		}
 		return new Response(
