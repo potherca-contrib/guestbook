a388a41e40c8e694baa3f1aecde952ce8b50eabb
Denis Žoljom <denis.zoljom@gmail.com>
2020-12-27 10:43:28 +0100

    Add pagination for the comments

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -7,6 +7,7 @@ use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
 use Twig\Error\{LoaderError, RuntimeError, SyntaxError};
@@ -15,15 +16,6 @@ class ConferenceController extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
-     *
-     * @param Environment $twig
-     * @param ConferenceRepository $conferenceRepository
-     *
-     * @return Response
-     *
-     * @throws LoaderError
-     * @throws RuntimeError
-     * @throws SyntaxError
      */
     public function index(Environment $twig, ConferenceRepository $conferenceRepository): Response
     {
@@ -35,11 +27,16 @@ class ConferenceController extends AbstractController
     /**
      * @Route("/conference/{id}", name="conference")
      */
-    public function show(Environment $twig, Conference $conference , CommentRepository $commentRepository): Response
+    public function show(Request $request, Environment $twig, Conference $conference , CommentRepository $commentRepository): Response
     {
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $commentRepository->getCommentPaginator($conference, $offset);
+
         return new Response($twig->render('conference/show.html.twig', [
             'conference' => $conference,
-            'comments' => $commentRepository->findBy(['conference' => $conference], ['createdAt' => 'DESC'])
+            'comments' => $paginator,
+            'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+            'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
         ]));
     }
 }
