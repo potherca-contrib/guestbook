15423189b06ec3f48cb9a9810e13b24bc11dd909
Denis Žoljom <denis.zoljom@gmail.com>
2021-01-03 10:34:22 +0100

    Add a functional test for the conference controller

---
+++ tests/Controller/ConferenceControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ConferenceControllerTest extends WebTestCase
+{
+    public function testSomething()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Give your feedback!');
+    }
+}
+symfony composer req orm-fixtures --dev
